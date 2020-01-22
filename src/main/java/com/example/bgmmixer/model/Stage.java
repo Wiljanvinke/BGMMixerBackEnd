611ddda705d87278f509de18e2c67df8ed013f6f
Wiljan Vinke <wiljanvinke@gmail.com>
2020-01-22 14:33:31 +0100

    Added noarg constructor, getter and setters

--- src/main/java/com/example/bgmmixer/model/Stage.java
+++ src/main/java/com/example/bgmmixer/model/Stage.java
@@ -15,4 +15,46 @@ public class Stage {
 
     @ManyToOne
     private Song song;
+
+    public Stage(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public void setSong(Song song) {
+        this.song = song;
+    }
 }
