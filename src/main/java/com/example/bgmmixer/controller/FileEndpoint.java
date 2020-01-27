137ad47f544b184fdcd7054fa87e82e4bbfdcc8b
Wiljan Vinke <wiljanvinke@gmail.com>
2020-01-27 13:11:21 +0100

    Disabled custom CORS filter

--- src/main/java/com/example/bgmmixer/controller/FileEndpoint.java
+++ src/main/java/com/example/bgmmixer/controller/FileEndpoint.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@CrossOrigin
 @RestController
 public class FileEndpoint {
 
--- src/main/java/com/example/bgmmixer/controller/SongEndpoint.java
+++ src/main/java/com/example/bgmmixer/controller/SongEndpoint.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/songs")
 public class SongEndpoint {
