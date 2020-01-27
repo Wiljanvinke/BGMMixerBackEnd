2b83de6af5725ce46db87b0226af3709738d1f05
Wiljan Vinke <wiljanvinke@gmail.com>
2020-01-27 13:48:35 +0100

    Updated FileDto

--- src/main/java/com/example/bgmmixer/dtos/FileDto.java
+++ src/main/java/com/example/bgmmixer/dtos/FileDto.java
@@ -1,5 +1,7 @@
 package com.example.bgmmixer.dtos;
 
+import com.example.bgmmixer.model.File;
+
 public class FileDto {
     private String fileName;
     private String fileType;
@@ -9,9 +11,33 @@ public class FileDto {
 
     }
 
-    public FileDto(String fileName, String fileType, long fileSize){
+    public FileDto(File file){
+        this.fileName = file.getFileName();
+        this.fileType = file.getFileType();
+        this.fileSize = file.getFileSize();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
         this.fileType = fileType;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
         this.fileSize = fileSize;
     }
 }
