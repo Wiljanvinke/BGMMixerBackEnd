66d4df9ecb4fc2cfd2e2ef1690453842ddf50e57
Wiljan Vinke <wiljanvinke@gmail.com>
2020-02-04 12:25:26 +0100

    storeFile() now uses saveAndFlush

--- src/main/java/com/example/bgmmixer/repositories/FileRepository.java
+++ src/main/java/com/example/bgmmixer/repositories/FileRepository.java
@@ -1,9 +1,10 @@
 package com.example.bgmmixer.repositories;
 
 import com.example.bgmmixer.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FileRepository extends CrudRepository<File, Long> {
+public interface FileRepository extends JpaRepository<File, Long> {
 }
