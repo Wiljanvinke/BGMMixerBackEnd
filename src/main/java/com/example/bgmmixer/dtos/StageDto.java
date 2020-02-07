b92903e4ce95869f48e8516ca923bce0d1693f04
Wiljan Vinke <wiljanvinke@gmail.com>
2020-02-07 12:57:56 +0100

    Added Constructor for StageDto

--- src/main/java/com/example/bgmmixer/dtos/StageDto.java
+++ src/main/java/com/example/bgmmixer/dtos/StageDto.java
@@ -1,11 +1,20 @@
 package com.example.bgmmixer.dtos;
 
-import com.example.bgmmixer.model.Song;
+import com.example.bgmmixer.model.Stage;
 
 public class StageDto {
 
+    private long id;
     private String name;
     private double startTime;
     private double endTime;
     private long songId;
+
+    public StageDto(Stage stage) {
+        this.id = stage.getId();
+        this.name = stage.getName();
+        this.startTime = stage.getStartTime();
+        this.endTime = stage.getEndTime();
+        this.songId = stage.getSong().getId();
+    }
 }
