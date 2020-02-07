19a10d29007f983588548af1494eedab3ecd86dd
Wiljan Vinke <wiljanvinke@gmail.com>
2020-02-07 12:36:57 +0100

    Made StageDto with fields

---
+++ src/main/java/com/example/bgmmixer/dtos/StageDto.java
@@ -0,0 +1,11 @@
+package com.example.bgmmixer.dtos;
+
+import com.example.bgmmixer.model.Song;
+
+public class StageDto {
+
+    private String name;
+    private double startTime;
+    private double endTime;
+    private long songId;
+}
