017bb49c2430ae3883d3432a69f9c0ec9556dc8a
Wiljan Vinke <wiljanvinke@gmail.com>
2020-02-21 15:37:20 +0100

    Added Javadoc and optimized imports

--- src/main/java/com/example/bgmmixer/controller/SongEndpoint.java
+++ src/main/java/com/example/bgmmixer/controller/SongEndpoint.java
@@ -1,20 +1,11 @@
 package com.example.bgmmixer.controller;
 
 import com.example.bgmmixer.dtos.SongDto;
-import com.example.bgmmixer.model.File;
 import com.example.bgmmixer.model.Song;
-import com.example.bgmmixer.payload.UploadFileResponse;
-import com.example.bgmmixer.service.FileService;
 import com.example.bgmmixer.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin
 @RestController
@@ -24,34 +15,64 @@ public class SongEndpoint {
     @Autowired
     private SongService songService;
 
+    /**
+     * Get a single Song with corresponding Id in Dtoformat
+     * @param songId Id to match a Playlist to
+     * @return Song in Dtoformat
+     */
     @GetMapping("/{songId}")
     public SongDto getSong(@PathVariable long songId){
         return songService.findSongById(songId);
     }
 
+    /**
+     * Get all Songs in the database
+     * @return a List of all Songs in Dtoformat
+     */
     @GetMapping()
     public Iterable<SongDto> getSongs(){
         return songService.findAllSongs();
     }
 
+    /**
+     * Post a new Song to the database
+     * @param newSong body of the request containing the values of the new Song
+     * @return new Song
+     */
     @PostMapping()
     public Song newSong(@RequestBody Song newSong){
         return songService.saveSong(newSong);
     }
 
+    /**
+     * Post a new Song to the database with an existing File in the database
+     * @param newSong body of the request containing the values of the new Song
+     * @param fileId Id of the File to attach to the Song
+     * @return ResponseEntity containing the newly posted Song in Dtoformat
+     */
     @PostMapping("/{fileID}")
-    public ResponseEntity<SongDto> newSong(@RequestBody Song newSong, @PathVariable long fileID){
-        return songService.saveSong(newSong, fileID);
+    public ResponseEntity<SongDto> newSong(@RequestBody Song newSong, @PathVariable long fileId){
+        return songService.saveSong(newSong, fileId);
     }
 
+    /**
+     * Update the Song with corresponding Id to include the values included in the body
+     * @param songId body of the request containing the new values of the Song
+     * @return ResponseEntity containing the newly updated Song in Dtoformat
+     */
     @PutMapping("/{songID}")
-    public ResponseEntity<SongDto> updateSong(@RequestBody SongDto songDto, @PathVariable long songID){
-        return songService.updateSong(songDto, songID);
+    public ResponseEntity<SongDto> updateSong(@RequestBody SongDto songDto, @PathVariable long songId){
+        return songService.updateSong(songDto, songId);
     }
 
+    /**
+     * Delete the Song with the corresponding Id
+     * @param songId Id of the song to delete
+     * @return ResponseEntity containing the deleted Song in DtoFormat
+     */
     @DeleteMapping("/{songID}")
-    public void deleteSong(@PathVariable long songID){
-        songService.deleteSong(songID);
+    public void deleteSong(@PathVariable long songId){
+        songService.deleteSong(songId);
     }
 
 
